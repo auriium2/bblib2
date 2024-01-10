b51697f71ec1272c727d397c65cb6e04d64b9f7a
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2024-01-10 11:03:48 -0700

    hasUpdated needs to be retained at runtime

--- mattlib2-api/src/main/java/xyz/auriium/mattlib2/log/HasUpdated.java
+++ mattlib2-api/src/main/java/xyz/auriium/mattlib2/log/HasUpdated.java
@@ -2,8 +2,13 @@ package xyz.auriium.mattlib2.log;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(METHOD)
+@Retention(RUNTIME)
 public @interface HasUpdated {
 
     String[] keysToCheck();
