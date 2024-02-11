4b417ab44c6a012489a5a64af8734cd5699041da
Matt Lui <92664603+superyuuki@users.noreply.github.com>
2024-02-10 18:26:14 -0700

    console component is a component

--- mattlib2-core/src/main/java/xyz/auriium/mattlib2/log/ConsoleComponent.java
+++ mattlib2-core/src/main/java/xyz/auriium/mattlib2/log/ConsoleComponent.java
@@ -3,7 +3,7 @@ package xyz.auriium.mattlib2.log;
 import xyz.auriium.mattlib2.log.annote.Log;
 import xyz.auriium.yuukonstants.exception.ExplainedException;
 
-public interface ConsoleComponent {
+public interface ConsoleComponent extends INetworkedComponent {
 
     @Log("console") void reportToConsole(String stuffToGoToTheConsole);
 
